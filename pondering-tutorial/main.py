67a493be3c89325093479c7354abea13683b5321
panda916 <67279281+panda916@users.noreply.github.com>
2024-01-09 14:50:52 +0700

    Update main.py

--- pondering-tutorial/main.py
+++ pondering-tutorial/main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from streamlit_option_menu import option_menu
 
-import about, account, home, trending, your_posts
+import about, home, trending, your_posts
 
 st.set_page_config(
         page_title="Pondering",
